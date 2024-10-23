2ac3c521d03385cf4b1ab76733e51799cf298096
Stas Samoylov <zakyska1301@gmail.com>
2024-10-23 23:56:12 +0300

    github response success

--- app/main.py
+++ app/main.py
@@ -1,18 +1,20 @@
 import os
 import logging
 
+import openai
 import httpx
 from fastapi import FastAPI, HTTPException
 from dotenv import load_dotenv
 from pydantic import BaseModel, HttpUrl
 
-logging.basicConfig(level=logging.INFO)
-
-app = FastAPI()
 load_dotenv()
+logging.basicConfig(level=logging.INFO)
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 GITHUB_API_TOKEN = os.getenv("GITHUB_API_TOKEN")
+
+client_openai = openai.Client(api_key=OPENAI_API_KEY)
+app = FastAPI()
 
 class ReviewRequest(BaseModel):
     description: str
@@ -30,8 +32,9 @@ async def review_code(request: ReviewRequest):
         logging.error(f"Error during review: {e}")
         raise HTTPException(status_code=500, detail="Internal server error")
 
-async def fetch_repo_contents(repo_url: str, token: str):
-    headers = {"Authorization": f"token {token}"}
+async def fetch_repo_contents(repo_url: str):
+    headers = {"Authorization": f"Bearer {GITHUB_API_TOKEN}",
+               "Accept": "application/vnd.github.raw+json"}
     api_url = f"https://api.github.com/repos/{repo_url}/contents"
     async with httpx.AsyncClient() as client:
         response = await client.get(api_url, headers=headers)
@@ -39,20 +42,19 @@ async def fetch_repo_contents(repo_url: str, token: str):
             raise HTTPException(status_code=500, detail="Error fetching repository data from GitHub")
         return response.json()
     
-async def analyze_code(code: str, openai_api_key: str):
-    headers = {
-        "Authorization": f"Bearer {openai_api_key}",
-        "Content-Type": "application/json"
-    }
+def analyze_code(code: str):
     data = {
-        "model": "gpt-4o",
+        "model": "gpt-4o-mini",
         "messages": [{"role": "user", "content": f"Analyze this code and provide a review: {code}"}],
-        "temperature" : 0.7
     }
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.post("https://api.openai.com/v1/chat/completions", data=data, headers=headers)
-            return response #.json()["choices"][0]["text"]
-        except Exception as e:
-            return e
+    try:
+        if httpx.Response.is_success:
+            chat = client_openai.chat.completions.create(messages=data["messages"], model=data['model'], stream=True)
+            for chunk in chat:
+                print(chunk.choices[0].delta.content, end="")
+        else:
+            raise HTTPException(status_code=429, detail="Error response OpenAI")
+    except:
+        raise HTTPException(status_code=500, detail="Error analyzing code with OpenAI")
+
 
--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,4 @@
-from app.main import analyze_code, OPENAI_API_KEY
+from app.main import analyze_code, fetch_repo_contents
 import asyncio
 
 code = '''import os
@@ -13,54 +13,14 @@ logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
 load_dotenv()
-
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-GITHUB_API_TOKEN = os.getenv("GITHUB_API_TOKEN")
-
-class ReviewRequest(BaseModel):
-    description: str
-    repo_url: HttpUrl
-    level: str
-
-@app.post("/review")
-async def review_code(request: ReviewRequest):
-    try:
-        logging.info("Review process started")
-        repo_data = await fetch_repo_contents(request.repo_url, GITHUB_API_TOKEN)
-        review = await analyze_code(repo_data, OPENAI_API_KEY)
-        return {"review": review}
-    except Exception as e:
-        logging.error(f"Error during review: {e}")
-        raise HTTPException(status_code=500, detail="Internal server error")
-
-async def fetch_repo_contents(repo_url: str, token: str):
-    headers = {"Authorization": f"token {token}"}
-    api_url = f"https://api.github.com/repos/{repo_url}/contents"
-    async with httpx.AsyncClient() as client:
-        response = await client.get(api_url, headers=headers)
-        if response.status_code != 200:
-            raise HTTPException(status_code=500, detail="Error fetching repository data from GitHub")
-        return response.json()
-    
-async def analyze_code(code: str, openai_api_key: str):
-    headers = {
-        "Authorization": f"Bearer {openai_api_key}",
-        "Content-Type": "application/json"
-    }
-    data = {
-        "model": "gpt-4-turbo",
-        "prompt": f"Analyze this code and provide a review: {code}",
-        "max_tokens": 500
-    }
-    async with httpx.AsyncClient() as client:
-        response = await client.post("https://api.openai.com/v1/completions", json=data, headers=headers)
-        if response.status_code != 200:
-            raise HTTPException(status_code=500, detail="Error analyzing code with OpenAI")
-        return response.json()["choices"][0]["text"]
 '''
 
-async def main():
-    print(await analyze_code(code, OPENAI_API_KEY))
+def test_analyze():
+    analyze_code(code)
+
+async def test_fetch_repo_contents():
+    print(await fetch_repo_contents('Stas-corp/Test_USD'))
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    # test_analyze()
+    asyncio.run(test_fetch_repo_contents())
